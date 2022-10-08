bed8720145c2cf20ba0b1e553ece374bacb3bc50
KabanFriends <yuukun2004@protonmail.ch>
2022-10-08 17:51:31 +0900

    update mod version

--- Common/PluginConstants.cs
+++ Common/PluginConstants.cs
@@ -6,6 +6,6 @@ namespace Qbeh1_SpeedrunMod.Common
     {
         public const string PLUGIN_GUID = "Qbeh1-SpeedrunMod";
         public const string PLUGIN_NAME = "Qbeh-1 Speedrun Mod";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "0.1.0";
     }
 }
